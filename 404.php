b3e946759859da7ff2461288e607ef13d3889ee0
Javithor360 <floresmejia004@gmail.com>
2021-08-30 20:42:16 -0600

    Correción error404

--- 404.php
+++ 404.php
@@ -6,17 +6,17 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link rel="shortcut icon" href="./src/logos/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="./css/error404.css">
+        <link rel="shortcut icon" href="../src/logos/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="../css/error404.css">
         <title>Página no encontrada ∙ La Manada</title>
     </head>
     <body>
         <div class="container">
             <h1>¡Algo salió mal!</h1>
-            <img src="./src/error404.png" height="500px" width="750px">
+            <img src="../src/error404.png" height="500px" width="750px">
             <div id="right">
                 <p>La página solicitada no ha sido encontrada...</p>
-                <a href="./index.html"><button>Regresar</button></a>
+                <a href="../php/principal.php"><button>Regresar</button></a>
             </div>
         </div>
     </body>
