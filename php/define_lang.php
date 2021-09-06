15d2cac3dfc5d7cc25a9ff1daf2fe30bbd6e07b7
Javithor360 <floresmejia004@gmail.com>
2021-09-05 22:19:30 -0600

    Fix

--- php/define_lang.php
+++ php/define_lang.php
@@ -3,6 +3,13 @@
     if(!isset($_SESSION['lang'])){
         $_SESSION['lang'] = 'es';
     }
+
+    if (!function_exists('str_contains')) {
+        function str_contains(string $haystack, string $needle): bool
+        {
+            return '' === $needle || false !== strpos($haystack, $needle);
+        }
+    }
     
     if(str_contains($_SERVER['SCRIPT_FILENAME'], '/pets/')){ $pathway = '../../'; }
     else if(str_contains($_SERVER['SCRIPT_FILENAME'], '/guides/')) { $pathway = '../'; }
